90a77f4f1514d184c7c439ef90ff99865d4053c6
Sergey Katsubo <skatsubo@gmail.com>
2025-08-19 01:07:56 +0300

    Support creating sandbox from existing Immich data and Postgres

--- README.md
+++ README.md
@@ -8,26 +8,42 @@ Test Immich upgrades in a sandbox to avoid surprise during a real upgrade.
 
 A tool/sandbox for testing Immich version upgrades.
 
-This tool automates validating upgrade paths:
-- It deploys the baseline (initial) version.
-- You do initial setup and add some files to that baseline setup.
-- Then it creates a backup.
-- Next, one by one, it tries requested upgrade paths.
-  - Each time from a fresh baseline instance restored from backup.
+This tool automates validation of upgrade paths: it deploys the initial version in a sandbox and performs an upgrade to the target version. (Post-upgrade e2e tests to be implemented in the future).
+
+Step by step execution of the script:
+
+1. Initial version.
+The sandbox can be created from scratch (empty), from an existing snapshot, from data of an existing Immich instance (library, postgres).
+The sandbox can be reviewed and configured or populated with assets before performing an upgrade.
+
+- It deploys the initial version.
+- You do initial setup and add some files to the sandbox.
+- Then it creates a snapshot.
+
+2. Refreshing the sandbox.
+
+Before each upgrade test a fresh sandbox instance is created/restored from a snapshot (step 1).
+
+3. Upgrade (to target version)
+
+- Next, one by one, it tries requested upgrade paths in a sandbox.
+  - Each time from a fresh test instance restored from snapshot.
   - Currently it tests two upgrade scenarios: (1) new version + old compose, (2) new version + new compose.
   - Apart from that, other modifications are possible in principle (e.g. testing custom changes in docker compose file) but not implemented yet.
 - After each upgrade it pauses and waits until you interact with the upgraded instance (check UI and logs) to validate the upgrade outcome.
 
 ## How to use
 
+Quick start:
+
 1. Download [test-upgrade.sh](https://raw.githubusercontent.com/skatsubo/immich-upgrade-lab/refs/heads/main/test-upgrade.sh) from Github and make the script executable: `chmod +x path/to/test-upgrade.sh`
 2. Go to an empty directory.
-3. Run the script with two arguments: "from" and "to" versions for testing upgrade between.
+3. Run the script with two arguments: "from" and "to" versions for testing upgrade between. Follow the on-screen instructions.
 
 Example:
 
 ```sh
-# test upgrade from v1.123.0 to v1.132.3
+# upgrade from v1.123.0 to v1.132.3
 ./test-upgrade.sh v1.123.0 v1.132.3
 ```
 
@@ -41,14 +57,28 @@ Check the usage instructions by providing `--help / -h` or simply run it without
 Immich upgrade in a sandbox
 
 Performs upgrade from version ver1 to version ver2 in a sandbox.
-It tests two scenarios: (1) new version + old compose, (2) new version + new compose.
+It tests two upgrade scenarios: (1) new version + old compose, (2) new version + new compose.
+
+The test instance (sandbox) can be created:
+  - from scratch (empty) using default/release compose
+  - from a previously created snapshot
+  - using existing Immich instance's compose+data+postgres
+By default, when no optional args are specified, the sandbox is created from this version's snapshot if exists, otherwise from scratch.
+
+Both versions (from and to) are mandatory arguments.
 
 Usage:
-  ./test-upgrade.sh <ver1> <ver2>                # Test upgrades from version1 to version2
-  ./test-upgrade.sh --from <ver1> --to <ver2>    # Test upgrades from version1 to version2 (flags form)
-  ./test-upgrade.sh --help                       # Show this help
+  ./test-upgrade.sh <ver1> <ver2>                   # Upgrade from version1 to version2
+  ./test-upgrade.sh --from <ver1> --to <ver2>       # Upgrade from version1 to version2 (flags form)
+  ./test-upgrade.sh <ver1> <ver2> [--args ...]      # Upgrade with extra args
+  ./test-upgrade.sh --help                          # Show this help
 
-Example:
+Optional arguments:
+  --from-compose <dir>    Use compose files from specified location when creating sandbox.
+  --from-data <dir>       Use (copy) Immich data (library) from specified location when creating sandbox.
+  --from-postgres <dir>   Use (copy) Postgres data from specified location when creating sandbox.
+
+Examples:
   ./test-upgrade.sh v1.123.0 v1.132.3
   ./test-upgrade.sh --from v1.123.0 --to v1.132.3
 ```
--- test-upgrade.sh
+++ test-upgrade.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+
+
+# --from-dir for using existing backup/origin
+#     Path to a current backup
+
+#     > Copy your/the current/existing backup to the local sandbox
+#     > Creating the "real" starting point without needing to manually load/configure anything
+
+#     origin/baseline type = { internal/managed/fresh, external }
+
+#     layout/tree of baseline source directory
+#         allow providing separately paths to
+#             library
+#             postgres
+#             compose
+
+
+
 #
 # shell
 #
@@ -9,6 +27,11 @@ set -e
 # configuration and constants
 #
 timeout_startup=120
+
+# dir for running sandbox (ephemeral/test instance undergoing upgrade from baseline to target)
+sandbox_dir='.'
+# dir for everything else: managed baselines/snapshots/origins and compose release files 
+cache_dir='.'
 
 #
 # aux functions
@@ -30,6 +53,7 @@ debug() {
 #
 # upgrade functions
 #
+
 warn() {
     echo "======================================================"
     echo "⚠️ Run this script from a dedicated empty directory. ⚠️"
@@ -47,26 +71,150 @@ wait_for_user() {
     read -n 1 -s -r
 }
 
-download_compose_files() {
-    for ver in "$@" ; do
-        log "Get compose and env from Immich release assets"
-        mkdir -p "release/$ver"
-        curl -sSL -o "release/$ver/docker-compose.yml" "https://github.com/immich-app/immich/releases/download/$ver/docker-compose.yml"
-        curl -sSL -o "release/$ver/.env" "https://github.com/immich-app/immich/releases/download/$ver/example.env"
-    done
-    ver=""
+# setup_dirs
+ensure_paths() {
+    # order of mkdir-vs-realpath matters because realpath on Macos does not have --canonicalize-missing "no path components need exist or be a directory"
+    mkdir -p "$sandbox_dir"
+    sandbox_dir_abs=$(realpath "$sandbox_dir")
+
+    mkdir -p "$cache_dir"
+    cache_dir_abs=$(realpath "$cache_dir")
+
+    # script_dir=$(dirname $(realpath -f "$0"))
+
+    snapshot_dir="$cache_dir_abs/snapshot/$from"
+
+    release_dir="$cache_dir_abs/release/$from"
+
+    sandbox_data_dir="$sandbox_dir_abs"/library
+    sandbox_postgres_dir="$sandbox_dir_abs"/postgres
 }
 
-provision_compose_files() {
-    log "Provision compose and env files"
-    cp "release/$ver/docker-compose.yml" "release/$ver/.env" .
-    echo "IMMICH_VERSION=$ver" >> .env
+download_release_compose_files() {
+    # TODO: unless exists
+    local ver
+    local release_dir
+    for ver in "$@" ; do
+        log "Get compose and env from Immich release $ver"
+
+        release_dir="$cache_dir_abs/release/$ver"
+
+        if [[ -f $release_dir/docker-compose.yml ]] && [[ -f $release_dir/.env ]] ; then
+            log "Compose files for $ver already exist in $release_dir. Nothing to do."
+        else
+            mkdir -p "$release_dir"
+            curl -sSL -o "$release_dir/docker-compose.yml" "https://github.com/immich-app/immich/releases/download/$ver/docker-compose.yml"
+            curl -sSL -o "$release_dir/.env" "https://github.com/immich-app/immich/releases/download/$ver/example.env"
+        fi
+    done
+}
+
+detemine_sandbox_sources() {
+    # if
+    #   no custom "from" source compose/data/postgres paths specified 
+    # and 
+    #   snapshot already exists at the default "snapshot/$version" path
+    # then use snapshot's content (copy its data/config when provisioning new sandbox)
+    if [[ -z ${from_compose}${from_data}${from_postgres} ]] && check_snapshot_exists ; then
+        source_type='snapshot'
+
+        source_compose_dir="$snapshot_dir"
+        source_data_dir="$snapshot_dir/library"
+        source_postgres_dir="$snapshot_dir/postgres"
+
+        log "Snapshot exists in '$snapshot_dir', use it as source when creating sandbox. (Remove the snapshot directory if you would like to start from scratch, then re-run this script)"
+    else
+        # when a directory is specified, use it
+        # otherwise for compose use the release version, for data/postgres assume they need to be created from scratch
+        if [[ -n ${from_data}${from_postgres} ]] ; then
+            source_type='custom'
+        else
+            source_type='scratch'
+        fi
+        source_compose_dir="${from_compose:-$release_dir}"
+        source_data_dir="${from_data:-}"
+        source_postgres_dir="${from_postgres:-}"
+    fi
+    log "Snapshot source type: $source_type"
+}
+
+provision_sandbox_cleanup() {
+    rm -rf "$sandbox_dir"/*compose*.y*ml
+    rm -rf "$sandbox_dir"/.env
+    # TODO: *.env, if exists, to avoid "No such file or directory"
+    # rm -rf "$sandbox_dir"/*.env
+    rm -rf "$sandbox_data_dir"
+    rm -rf "$sandbox_postgres_dir"
+}
+
+provision_sandbox_compose() {
+    local ver="$1"
+    local source_dir="$2"
+    local dest_dir="$3"
+
+    log "Provision compose and env files from: $source_dir"
+
+    cp "$source_dir"/*compose*.y*ml "$dest_dir"
+    cp "$source_dir"/.env           "$dest_dir"
+    # TODO: *.env, if exists, to avoid "No such file or directory". Or rsync.
+    # cp "$source_dir"/*.env "$dest_dir"
+
+    echo "IMMICH_VERSION=$ver" >> "$dest_dir"/.env
+}
+
+# copy_content between dirs
+provision_sandbox_content() {
+    local content="$1"
+    local source_dir="$2"
+    local dest_dir="$3"
+    log "Provision $content from: $source_dir"
+
+    mkdir -p "$dest_dir"
+    cp -r "$source_dir"/. "$dest_dir"
+}
+
+# provision sandbox from sources (if any) or use empty/default
+provision_sandbox() {
+    log "Provision sandbox from: $source_type"
+
+    # clean up first
+    provision_sandbox_cleanup
+
+    # compose
+    provision_sandbox_compose "$from" "$source_compose_dir" "$sandbox_dir"
+
+    # data
+    if [[ -n $source_data_dir ]]; then
+        provision_sandbox_content data "$source_data_dir" "$sandbox_data_dir"
+    else
+        log "Provision empty data dir"
+    fi
+
+    # postgres
+    if [[ -n $source_postgres_dir ]]; then
+        provision_sandbox_content postgres "$source_postgres_dir" "$sandbox_postgres_dir"
+    else
+        log "Provision empty postgres dir"
+    fi
+
+    deploy_stack
+}
+
+# TODO: handling of $source_type to make code DRY
+provision_sandbox_from_snapshot() {
+    source_type="snapshot"
+
+    source_compose_dir="$snapshot_dir"
+    source_data_dir="$snapshot_dir/library"
+    source_postgres_dir="$snapshot_dir/postgres"
+
+    provision_sandbox
 }
 
 deploy_stack() {
     log "These images will be used:"
     docker compose config | grep "image:"
-        # Expected output includes current/baseline version to be upgraded from
+        # Expected output includes current version to be upgraded from
         # image: docker.io/tensorchord/pgvecto-rs:pg14-v0.2.0@sha256:90724186f0a3517cf6914295b5ab410db9ce23190a2d9d0b9dd6463e3fa298f0
         # image: ghcr.io/immich-app/immich-machine-learning:v1.123.0
         # image: ghcr.io/immich-app/immich-server:v1.123.0
@@ -84,65 +232,106 @@ teardown() {
     rm -rf ./library ./postgres .env docker-compose.yml docker-compose.override.yml
 }
 
-setup_baseline_initial () {
-    # on baseline version: do initial setup, upload a few photos and so on
-    wait_for_user "This setup will be used as a baseline / initial version for upgrades.\nGo to http://localhost:2283 and do initial configuration, upload photos, check if it looks good."
-
-    # stop, backup, destroy the baseline
-    log "Create backup"
-    docker compose stop
-    mkdir -p "$ver"
-    cp -r ./library ./postgres .env docker-compose.yml "$ver"
-
-    teardown
+setup_sandbox_initial () {
+    # heredoc is indented with tabs (not spaces) to match code indentation
+    local msg=$(cat <<-'EOF'
+		This setup will be used as a baseline/snapshot for upgrades.
+		Go to http://localhost:2283 and do initial configuration, upload photos, check if it looks good.
+		EOF
+    )
+    wait_for_user "$msg"
 }
 
-check_backup_exists() {
-    if [[ -d "$ver"/postgres ]] && [[ -d "$ver"/library ]] && [[ -f "$ver"/.env ]] && [[ -f "$ver"/docker-compose.yml ]] ; then
+# internal baseline exists
+check_snapshot_exists() {
+    # if [[ -d "$ver"/postgres ]] && [[ -d "$ver"/library ]] && [[ -f "$ver"/.env ]] && [[ -f "$ver"/docker-compose.yml ]] ; then
+    if [[ -f "$snapshot_dir/state.ok" ]]; then
         return 0
     fi
     return 1
 }
 
-provision_from_backup() {
-    log "Provision stack from backup"
-    log "Restore configs and data directories (library, postgres)"
-    rm -rf ./library ./postgres .env docker-compose.yml docker-compose.override.yml
-    cp -r "$ver"/* "$ver"/.env .
+make_snapshot() {
+    # stop, backup
+    log "Create snapshot"
+    docker compose stop
 
-    deploy_stack
+    # TODO: better handling, do not overwrite in certain scenarios
+    if check_snapshot_exists ; then
+        log "Snapshot at $snapshot_dir exists. Overwriting."
+    fi
+
+    mkdir -p "$snapshot_dir"
+
+    # TODO: proper vars snapshot_dir vs snapshot_data_dir / snapshot_postgres_dir, etc
+    # TODO: cleanup before copying
+    # TODO: use function instead of cp
+    cp -r "$sandbox_data_dir" "$sandbox_postgres_dir" "$snapshot_dir"
+    cp "$sandbox_dir"/*compose*.y*ml "$snapshot_dir"
+    cp "$sandbox_dir"/.env           "$snapshot_dir"
+    # TODO: *.env, if exists, to avoid "No such file or directory". Or rsync.
+    # cp "$sandbox_dir"/*.env          "$snapshot_dir"
+
+    touch "$snapshot_dir/state.ok"
 }
 
 wait_for_immich_running() {
     log "Wait for Immich to become ready"
-    container="immich_server"
+
+    local container="immich_server"
+    local is_healthy=''
     for i in $(seq 1 $((timeout_startup/2))); do
         status=$(docker inspect --format='{{.State.Health.Status}}' "$container")
         if [[ $status == "healthy" ]]; then
-            echo "Container is healthy."
+            debug "Container is healthy."
+            is_healthy="true"
             break
         else
             printf "."
         fi
         sleep 2
     done
+    if [[ -n $is_healthy ]]; then
+        echo ok
+    else
+        echo 
+        log "WARN: container $container is not healthy after $timeout_startup sec. Check sandbox's logs and retry. Also this may be a bug in the script. Exiting."
+    fi
 }
 
 #
 # command line functions
 #
 cli_print_help() {
+    echo
     echo "Immich upgrade in a sandbox"
     echo
     echo "Performs upgrade from version ver1 to version ver2 in a sandbox."
-    echo "It tests two scenarios: (1) new version + old compose, (2) new version + new compose."
+    echo "It tests two upgrade scenarios: (1) new version + old compose, (2) new version + new compose."
+    echo
+    echo "The test instance (sandbox) can be created:"
+    echo "  - from scratch (empty) using default/release compose"
+    echo "  - from a previously created snapshot"
+    echo "  - using existing Immich instance's compose+data+postgres"
+    echo "By default, when no optional args are specified, the sandbox is created from this version's snapshot if exists, otherwise from scratch."
+    echo
+    echo "Both versions (from and to) are mandatory arguments."
     echo
     echo "Usage:"
-    echo "  $0 <ver1> <ver2>                # Test upgrades from version1 to version2"
-    echo "  $0 --from <ver1> --to <ver2>    # Test upgrades from version1 to version2 (flags form)"
-    echo "  $0 --help                       # Show this help"
+    echo "  $0 <ver1> <ver2>                   # Upgrade from version1 to version2"
+    echo "  $0 --from <ver1> --to <ver2>       # Upgrade from version1 to version2 (flags form)"
+    echo "  $0 <ver1> <ver2> [--args ...]      # Upgrade with extra args"
+    echo "  $0 --help                          # Show this help"
     echo
-    echo "Example:"
+    echo "Optional arguments:"
+    echo "  --from-compose <dir>    Use compose files from specified location when creating sandbox."
+    echo "  --from-data <dir>       Use (copy) Immich data (library) from specified location when creating sandbox."
+    echo "  --from-postgres <dir>   Use (copy) Postgres data from specified location when creating sandbox."
+    # echo "  --from-compose-release  [WIP] Force using release compose files for sandbox even if compose files are present in the baseline."
+    # echo "                          By default release files are used only when creating a fresh sandbox from scratch or when missing in source."
+    # echo "  --from-dir <...>        [WIP] Create sandbox using a copy of existing data (library, postgres) in <dir>."
+    echo
+    echo "Examples:"
     echo "  $0 v1.123.0 v1.132.3"
     echo "  $0 --from v1.123.0 --to v1.132.3"
     echo
@@ -153,14 +342,20 @@ parse_args() {
 
     while [[ "$#" -gt 0 ]]; do
         case "$1" in
-            --from)    from="$2"; shift 2 ;;
-            --to)      to="$2"; shift 2 ;;
-            --help|-h) cli_print_help; exit 0 ;;
-            *)         from="$1"; to="$2"; break ;;
+            --from)          from="$2"; from_version="$2"; shift 2 ;;
+            --from-compose)  from_compose="$2"; shift 2 ;;
+            --from-compose-release) from_compose_release=1; shift ;;
+            --from-data)     from_data="$2" ; shift 2 ;;
+            --from-postgres) from_postgres="$2" ; shift 2 ;;
+            --to)            to="$2"; to_version="$2" ; shift 2 ;;
+            --help|-h)       cli_print_help; exit 0 ;;
+            # *)               from="$1"; to="$2"; break ;;
+            *)               from="$1"; to="$2" ; shift 2 ;;
         esac
     done
 
     if [[ -z "$from" || -z "$to" ]]; then
+        echo "Both versions 'from' and 'to' are required."
         cli_print_help
         exit 1
     fi
@@ -169,45 +364,60 @@ parse_args() {
 #
 # variables
 #
-ver=""
+
+# mainly for logging
+ver=''
+
+snapshot_dir=''
+
+# sandbox source type: scratch (fresh, empty data and postgres), snapshot, custom (external, provided path)
+source_type="scratch"
 
 #
 # main
 #
 parse_args "$@"
-
 log "Testing upgrade: $from -> $to"
-
 warn
 
-log "Get compose+env for requested versions. You can review/compare them."
-download_compose_files $from $to
-
-# prepare baseline
+# set dir/path variables, resolve/canonicalize, create dirs if not exist
+# this must be done before "cd anywhere" because args may contain relative paths
 ver="$from"
-teardown
-log "Setting up baseline version $ver to be tested for upgrades"
-if ! check_backup_exists ; then
-    log "Install and configure baseline"
-    provision_compose_files
-    deploy_stack
-    setup_baseline_initial
-else
-    log "Backup exists in '$ver', use it as baseline. (Remove '$ver' directory if you would like to start from scratch, then re-run this script)"
+ensure_paths
+
+# use sandbox dir as the current/work dir for all following tasks/functions
+# for now assume cwd is the sandbox dir
+# TODO: support "cd anywhere"
+# cd "$sandbox_dir"
+
+ver=''
+log "Get compose+env for requested versions. You can review/compare them."
+download_release_compose_files "$from" "$to"
+
+# prepare sandbox
+ver="$from"
+detemine_sandbox_sources
+log "Setting up a sandbox $ver to be tested for upgrades. Sandbox data will be populated from: $source_type."
+if docker compose config 2>&1 >/dev/null ; then
+  teardown
 fi
+provision_sandbox
+setup_sandbox_initial
+make_snapshot
+teardown
 
 # upgrade path 1: "from" -> "to" using compose of "from"
 ver="$from"
 echo
 log "🟡"
-log "🟡 [Test 1] Upgrade to $to using the existing/current docker-compose.yml (released in $from)"
+log "🟡 [Test 1] Upgrade to $to using the existing/current docker-compose.yml (matching $from)"
 log "🟡"
-provision_from_backup
+provision_sandbox_from_snapshot
 wait_for_immich_running
 
 ver="$to"
-log "Set target IMMICH_VERSION=$ver, but keep the existing compose of the current version $from"
-echo "IMMICH_VERSION=$ver" >> .env
+log "Begin upgrade. Set target IMMICH_VERSION=$ver, but keep the existing compose of the current version $from"
+echo "IMMICH_VERSION=$ver" >> "$sandbox_dir"/.env
 deploy_stack
 wait_for_user "Upgrade to $to done. Verify it: upload photos, check logs for errors."
 teardown
@@ -218,12 +428,12 @@ echo
 log "🟡"
 log "🟡 [Test 2] Upgrade to $to using docker-compose.yml for the target version (released in $to)"
 log "🟡"
-provision_from_backup
+provision_sandbox_from_snapshot
 wait_for_immich_running
 
 ver="$to"
-log "Use compose for the target version"
-provision_compose_files
+log "Begin upgrade. Use compose of the target version (released in $to)"
+provision_sandbox_compose "$to" "$release_dir" "$sandbox_dir"
 deploy_stack
 wait_for_user "Upgrade to $to done. Verify it: upload photos, check logs for errors."
 teardown
